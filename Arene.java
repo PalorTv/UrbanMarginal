027a30acc388f7b946cb5a2eaf9159a5165824c8
PalorTv <enzo.baumgartner36@gmail.com>
2025-02-24 09:45:43 +0100

    Phase 8 : ajout des murs

---
+++ Arene.java
@@ -0,0 +1,108 @@
+package vue;
+
+import java.awt.Dimension;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import controleur.Global;
+
+/**
+ * frame de l'arène du jeu
+ * @author emds
+ *
+ */
+public class Arene extends JFrame implements Global {
+
+	/**
+	 * Panel général
+	 */
+	private JPanel contentPane;
+	
+	/**
+	 * Panel contenant les murs
+	 */
+	private JPanel jpnMurs;
+	
+	/**
+	 * Zone de saisie du t'chat
+	 */
+	private JTextField txtSaisie;
+	/**
+	 * Zone d'affichage du t'chat
+	 */
+	private JTextArea txtChat ;
+	
+	/**
+	 * @return the jpnMurs
+	 */
+	public JPanel getJpnMurs() {
+		return jpnMurs;
+	}
+	
+	/**
+	 * @param jpnMurs the jpnMurs to set
+	 */
+	public void setJpnMurs(JPanel jpnMurs) {
+		this.jpnMurs.add(jpnMurs);
+		this.jpnMurs.repaint();
+	}
+	
+	/**
+	 * Ajoute un mur dans le panel des murs
+	 */
+	public void ajoutMurs(Object unMur) {
+		jpnMurs.add((JLabel)unMur);
+		jpnMurs.repaint();
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Arene() {
+		// Dimension de la frame en fonction de son contenu
+		this.getContentPane().setPreferredSize(new Dimension(LARGEURARENE, HAUTEURARENE + 25 + 140));
+	    this.pack();
+	    // interdiction de changer la taille
+		this.setResizable(false);
+		
+		setTitle("Arena");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		jpnMurs = new JPanel();
+		jpnMurs.setBounds(0, 0, LARGEURARENE, HAUTEURARENE);
+		jpnMurs.setOpaque(false);
+		jpnMurs.setLayout(null);
+		contentPane.add(jpnMurs);
+	
+		txtSaisie = new JTextField();
+		txtSaisie.setBounds(0, 600, 800, 25);
+		contentPane.add(txtSaisie);
+		txtSaisie.setColumns(10);
+		
+		JScrollPane jspChat = new JScrollPane();
+		jspChat.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		jspChat.setBounds(0, 625, 800, 140);
+		contentPane.add(jspChat);
+		
+		txtChat = new JTextArea();
+		jspChat.setViewportView(txtChat);
+		
+		JLabel lblFond = new JLabel("");
+		URL resource = getClass().getClassLoader().getResource(FONDARENE);
+		lblFond.setIcon(new ImageIcon(resource));		
+		lblFond.setBounds(0, 0, 800, 600);
+		contentPane.add(lblFond);
+		
+	}
+
+}
